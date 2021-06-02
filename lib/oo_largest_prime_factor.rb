a1051e95f16a6670698c7797e80222db36177e8c
Julian <julianrosenthal@yahoo.com>
2021-06-02 16:35:07 -0700

    commit tree 3f31a128e4f89b7954e2b0cf92b449a737e46a94

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_accessor :number
+
+  def initialize(limit)
+    @number = largest_prime_factor(limit)
+  end
+  
+  def largest_prime_factor(limit)
+    num = limit
+    max = -1
+    
+    while num % 2 === 0
+      num /= 2
+    end
+
+    idx = 3
+    while idx < (Math.sqrt(num) + 1)
+      if num % idx === 0
+        max = idx
+        num /= idx
+      else
+        idx += 2
+      end
+    end
+    max = num if num > 2
+    
+    return max
+  end
+
+
+end
