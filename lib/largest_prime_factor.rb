ca0d58c6b72ad5873aad292473d9fca24348964c
Julian <julianrosenthal@yahoo.com>
2021-06-02 16:35:15 -0700

    all tests passing

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+def largest_prime_factor(n)
+  max = -1
+
+  while n % 2 == 0
+    max = 2
+    n /= 2
+  end
+  
+  idx = 3
+  while idx < (Math.sqrt(n) + 1)
+    if n % idx === 0
+      max = idx
+      n /= idx
+    else
+      idx += 2
+    end
+  end
+  max = n if n > 2
+  return max
+end
+
